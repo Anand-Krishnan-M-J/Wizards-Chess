ad5592e14c0d1a6a99cf11f44174def565088a9b
Anand Krishnan M J <anandkrish0646@gmail.com>
2023-09-01 06:56:38 +0530

    Enable basic analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { Analytics } from '@vercel/analytics/react'
+import { store } from './store/index.ts'
 import App from './App.tsx'
 import './index.css'
-import { Provider } from 'react-redux'
-import { store } from './store/index.ts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <Provider store={store}>
         <React.StrictMode>
             <App />
+            <Analytics />
         </React.StrictMode>
     </Provider>
 )
